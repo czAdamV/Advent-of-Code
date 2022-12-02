8a563040164ea2532cac5715e87b8d2872701762
Adam Volek <volek@adamv.cz>
2022-12-03 00:42:25 +0100

    Run different days as subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use std::env;
+
+fn main() -> Result<(), String> {
+    match env::args().nth(1) {
+        Some(day) => Err(format!("Špatný den {} 😡", day))?,
+        None => Err("Chybí den 🥺")?
+    }
+}
